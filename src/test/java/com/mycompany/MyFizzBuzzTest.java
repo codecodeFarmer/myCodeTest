2773a7f116ccc30297709ac6121cebc8ffcf8875
unknown <huangxiaofeng62@126.com>
2019-06-27 22:03:59 +0800

    first commit

---
+++ src/test/java/com/mycompany/MyFizzBuzzTest.java
@@ -0,0 +1,24 @@
+package com.mycompany;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @ClassName: MyFizzBuzz
+ * @Description:FizzBuzz替换测试类
+ * @author 黄晓锋
+ * @date 2019-06-27 下午6:35:17
+ * @version V1.0
+ */
+public class MyFizzBuzzTest {
+
+	/**
+	 * <p>测试MyFizzBuzz的insteadSuitFizzBuzz方法</p>
+	 */
+	@Test
+    public void Test(){
+		Assert.assertEquals(MyFizzBuzz.insteadSuitFizzBuzz(3), "Fizz");   
+		Assert.assertEquals(MyFizzBuzz.insteadSuitFizzBuzz(5), "Buzz");  
+		Assert.assertEquals(MyFizzBuzz.insteadSuitFizzBuzz(15), "FizzBuzz");   
+    }
+}
